3550d1cba903256e18ba2c31862108fdd978cb4f
Noman01 <neamulhaque2002@gmail.com>
2022-08-02 23:09:05 +0600

    navbar links updated

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import './App.css';
+import { Routes } from "react-router-dom"
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
+      <Routes>
 
+      </Routes>
     </div>
   );
 }
