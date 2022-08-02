fea8457afa770ea624a80ab908725c46a1e95424
Noman01 <neamulhaque2002@gmail.com>
2022-08-03 03:13:11 +0600

    navbar done

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,14 +3,14 @@ import { Link } from 'react-router-dom';
 
 const Navbar = () => {
     return (
-        <div className=' bg-base-100 flex justify-center'>
-            <div class="navbar container">
-                <div class="navbar-start">
-                    <div class="dropdown">
-                        <label tabindex="0" class="btn btn-ghost lg:hidden">
-                            <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+        <div className=' bg-[#606060] flex justify-center'>
+            <div className="navbar container  text-white">
+                <div className="w-1/6 navbar-start">
+                    <div className="dropdown">
+                        <label tabIndex="0" className="btn btn-ghost lg:hidden">
+                            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                         </label>
-                        <ul tabindex="0" class="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+                        <ul tabIndex="0" className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
                             <Link to={'/'} as='li'>Home</Link>
                             <Link to={'/'} as='li'>Calls</Link>
                             <Link to={'/'} as='li'>Deals</Link>
@@ -19,10 +19,10 @@ const Navbar = () => {
                             <Link to={'/'} as='li'>Library</Link>
                         </ul>
                     </div>
-                    <Link to={'/'} class="btn btn-ghost normal-case text-xl">BsHappy</Link>
+                    <Link to={'/'} className="btn btn-ghost normal-case text-xl">BsHappy</Link>
                 </div>
-                <div class="navbar-center hidden lg:flex">
-                    <ul class="menu menu-horizontal p-0">
+                <div className="navbar-center hidden lg:flex">
+                    <ul className="menu menu-horizontal p-0 gap-6">
                         <Link to={'/'} as='li'>Home</Link>
                         <Link to={'/'} as='li'>Calls</Link>
                         <Link to={'/'} as='li'>Deals</Link>
@@ -31,8 +31,21 @@ const Navbar = () => {
                         <Link to={'/'} as='li'>Library</Link>
                     </ul>
                 </div>
-                <div class="navbar-end">
-                    <Link to={'/myAccount'} class="btn">My Account</Link>
+                <div className="navbar-end">
+                    <div className="form-control">
+                        <div className="input-group relative">
+                            <input type="text" placeholder="Search…" className="input input-bordered bg-[#606060] border-white border-4 placeholder-white" style={{ borderRadius: '2rem' }} />
+                            <button className="btn btn-square btn-ghost absolute right-0">
+                                <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 text-white" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" /></svg>
+                            </button>
+                        </div>
+                    </div>
+                    <Link to={'/myAccount'} className="btn btn-ghost">My Account</Link>
+                    <div className="avatar">
+                        <div className="w-8 rounded-full bg-white text-black justify-center items-center" style={{ display: 'flex' }}>
+                            <span className="font-bold w-full">?</span>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
